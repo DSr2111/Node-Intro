a111972a37c9aa379b780755277037578c26c193
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-05-16 07:38:30 -0400

    minor fixes

--- step1.js
+++ step1.js
@@ -2,12 +2,12 @@ const fs = require("fs");
 const process = require("process");
 
 function cat(path) {
-    fs.readFile(path, 'utf8', function(err, data) {
-        if (err) {
-            console.error(`Error reading ${path}: ${err}`;
-            process.exit(1);
-        } else {
-            console.log(data);
-        }
+  fs.readFile(path, "utf8", function (err, data) {
+    if (err) {
+      console.error(`Error reading ${path}: ${err}`);
+      process.exit(1);
+    } else {
+      console.log(data);
     }
+  });
 }
